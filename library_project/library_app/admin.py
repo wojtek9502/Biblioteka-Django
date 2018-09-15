2cbe6c6f35d2c176c351caf3c97014f2cd72a4fe
wojtek <wojtek.klusek2@gmail.com>
2018-09-15 17:09:51 +0200

    add data models

--- library_project/library_app/admin.py
+++ library_project/library_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from library_app import models
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','show_authors','slug','isbn','category','edition','publishing_house','publish_year','is_borrowed')
+    search_fields = ('title', 'authors')
+    raw_id_fields = ('authors', 'category', 'publishing_house')
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ('is_borrowed',)
+
+    def show_authors(self, obj):
+        return ", ".join([author_entry.last_name for author_entry in obj.authors.all()]) #zwroc , <nazwisko autora> dla kazdego autora w ksiazce
+    show_authors.short_description = 'Authors'
+
+admin.site.register(models.Book, BookAdmin)
+admin.site.register(models.BookCopy)
+admin.site.register(models.Author)
+admin.site.register(models.Category)
+admin.site.register(models.PublishingHouse)
+admin.site.register(models.Borrow)
--- library_project/library_app/models.py
+++ library_project/library_app/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+    
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.last_name + " " + self.first_name
+    
+
+class PublishingHouse(models.Model):
+    name = models.CharField(max_length=400)
+    city = models.CharField(max_length=300)
+    street = models.CharField(max_length=300)
+    house_number = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+    authors = models.ManyToManyField(Author, related_name='book_authors')
+    category = models.ForeignKey(Category, related_name='book_categories')
+    publishing_house = models.ForeignKey(PublishingHouse, related_name='publishing_houses')
+    isbn = models.CharField(max_length=30, unique=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default='')
+    publish_year = models.DateField(auto_now=False)
+    edition = models.IntegerField(default=1)
+    is_borrowed = models.BooleanField(default=False)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_single', kwargs={"slug": self.slug})
+
+class BookCopy(models.Model):
+    book = models.ForeignKey(Book)
+
+class Borrow(models.Model):
+    user = models.ForeignKey(User)
+    book_copy = models.ForeignKey(BookCopy)
+    borrow_date = models.DateField(auto_now=True)
+    receive_date = models.DateField(auto_now=False)
+
+    def __str__(self):
+        return self.user.username
+
+    
