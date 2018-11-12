4311a86d9ca56cadb8a06eaa3494ca119085d4da
wojtek <wojtek.klusek2@gmail.com>
2018-11-12 16:33:51 +0100

    DEBUG flag = False
    close #16

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '#y5yg^0$#u4-kr-^-8j7#kg4(a(-_5d5nfr2t-d86dicdvu^tk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['wojtek9502.pythonanywhere.com', '127.0.0.1', 'localhost']
 
